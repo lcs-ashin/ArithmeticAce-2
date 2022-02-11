de7d337714200f219f5f28bb74599799534fc206
lcs-ashin <ashin@lcs.on.ca>
2022-02-11 15:33:31 -0500

    Created DivisionView.

---
+++ ArithmeticAce/DivisionView.swift
@@ -0,0 +1,131 @@
+//
+//  DivisionnView.swift
+//  ArithmeticAce
+//
+//  Created by Russell Gordon on 2022-02-07.
+//
+
+import SwiftUI
+
+struct DivisionView: View {
+    
+    // MARK: Stored properties
+    @State var dividend = Int.random(in: 1...12)
+    @State var divisor = Int.random(in: 1...12)
+    @State var inputGiven = ""
+    
+    // Has an answer been checked?
+    @State var answerChecked = false
+    
+    // Was the answer given actually correct?
+    @State var answerCorrect = false
+    
+    // MARK: Computed properties
+    // What is the correct product?
+    var correctQuotient: Int {
+        return dividend / divisor
+    }
+    
+    var body: some View {
+                
+        VStack(spacing: 0) {
+            HStack {
+                Text("÷")
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("\(dividend)")
+                    Text("\(divisor)")
+                }
+            }
+            
+            Divider()
+            
+            HStack {
+                
+                ZStack {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                        //        CONDITION      true  false
+                        .opacity(answerCorrect ? 1.0 : 0.0)
+                    
+                    Image(systemName: "x.square")
+                        .foregroundColor(.red)
+                        //        CONDITION
+                        .opacity(answerCorrect == false && answerChecked == true ? 1.0 : 0.0)
+                }
+                
+                Spacer()
+                TextField("",
+                          text: $inputGiven)
+                    .multilineTextAlignment(.trailing)
+            }
+            
+            ZStack {
+                Button(action: {
+                    
+                    // Answer has been checked!
+                    answerChecked = true
+                    
+                    // Convert the input given to an integer, if possible
+                    guard let productGiven = Int(inputGiven) else {
+                        // Sadness, not a number
+                        answerCorrect = false
+                        return
+                    }
+
+                    // Check the answer!
+                    if productGiven == correctQuotient {
+                        // Celebrate! 👍🏼
+                        answerCorrect = true
+                    } else {
+                        // Sadness, they gave a number, but it's correct 😭
+                        answerCorrect = false
+                    }
+                }, label: {
+                    Text("Check Answer")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                    .opacity(answerChecked == false ? 1.0 : 0.0)
+                
+                Button(action: {
+                    
+                    // Generate new numbers
+                    dividend = Int.random(in: 1...12)
+                    divisor = Int.random(in: 1...12)
+                    
+                    // Reset properties that we are using to keep track of whether a question has been answered... and, whether the answer is correct
+                    answerChecked = false
+                    answerCorrect = false
+                    
+                    // Reset the field where the user gives an answer
+                    inputGiven = ""
+                    
+                }, label: {
+                    
+                    Text("New Question")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                    .opacity(answerChecked ? 1.0 : 0.0)
+            }
+            
+            
+            Spacer()
+        }
+        .padding(.horizontal)
+        .font(.system(size: 72))
+
+        
+    }
+}
+
+struct DivisionView_Previews: PreviewProvider {
+    static var previews: some View {
+        DivisionView()
+    }
+}
